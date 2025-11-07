8724a46ecd83ff6d6f36c851422fc030bb918e25
nam.nguyenphuong5497@gmail.com <nguyenthanhlong10122001@gmail.com>
2025-11-07 11:05:36 +0700

    update

--- logger/logger.go
+++ logger/logger.go
@@ -1,24 +1,28 @@
 package logger
 
 import (
+	"os"
+
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
 type LoggerI interface {
-
 	Info(msg string, args ...interface{})
 	Warn(msg string, args ...interface{})
 	Error(msg string, args ...interface{})
 	Debug(msg string, args ...interface{})
 	Fatal(msg string, args ...interface{})
 	Infof(template string, args ...interface{})
-	Debugf(template string, args ...interface{})	
+	Debugf(template string, args ...interface{})
 	Errorf(template string, args ...interface{})
 	Warnf(template string, args ...interface{})
 	Fatalf(template string, args ...interface{})
 }
 
+var (
+	level = zap.NewAtomicLevel()
+)
 
 type Logger struct {
 	*zap.SugaredLogger
@@ -26,14 +30,16 @@ type Logger struct {
 
 func NewLogger() *Logger {
 	// set caller skip to 2
-	cfg := zap.NewProductionConfig()
-	
-	cfg.Level.SetLevel(zapcore.DebugLevel) // Set the desired level (e.g., InfoLevel)
-	logger, _ := cfg.Build()
-	logger = logger.WithOptions(zap.AddCallerSkip(1))
-	
+
+	logger := zap.New(zapcore.NewCore(
+		zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
+		zapcore.AddSync(zapcore.Lock(zapcore.NewMultiWriteSyncer(os.Stderr))),
+		level,
+	), zap.AddCaller(), zap.AddCallerSkip(1),
+	)
+
 	sugar := logger.Sugar()
-	
+
 	return &Logger{
 		SugaredLogger: sugar,
 	}
@@ -69,11 +75,19 @@ func (l *Logger) Errorf(template string, args ...interface{}) {
 }
 func (l *Logger) Warnf(template string, args ...interface{}) {
 	l.SugaredLogger.Warnf(template, args...)
-}	
+}
 func (l *Logger) Fatalf(template string, args ...interface{}) {
 	l.SugaredLogger.Fatalf(template, args...)
 }
 
 var (
-	Log  LoggerI = NewLogger()
+	Log LoggerI = NewLogger()
 )
+
+func SetLevel(l string) {
+	zapLevel, err := zapcore.ParseLevel(l)
+	if err != nil {
+		zapLevel = zapcore.InfoLevel
+	}
+	level.SetLevel(zapLevel)
+}
